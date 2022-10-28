d1cbc692973053ee68bcc6f8baf8cf50e5476ab8
CodeGym <mr.hoanh19111998@gmail.com>
2022-10-28 08:23:12 +0700

    update

---
+++ ClassAccount.js
@@ -0,0 +1,22 @@
+class Account {
+    username;
+    password;
+    confirmpassword;
+    email;
+    phone;
+    address;
+
+    constructor(username, password, confirmpassword, email, phone, address) {
+        this.username = username;
+        this.password = password;
+        this.confirmpassword = confirmpassword;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+}
+
+
+
+
+
